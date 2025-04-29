4ffc8cd09a7253c03f202fb426d2fa6e6b6cffd1
SkublicsImre <imre.skublics@gmail.com>
2025-04-29 15:54:54 +0200

    Add gpio directory, and set up task construction

---
+++ ESP32-S3R16V Jtag/main/gpio.c
@@ -0,0 +1,35 @@
+#define A_1  GPIO_NUM_1 //ADC1
+#define A_2  GPIO_NUM_2 //ADC1 
+#define A_3  GPIO_NUM_3 //ADC1
+#define A_4  GPIO_NUM_4 //ADC1
+#define A_5  GPIO_NUM_5 //ADC1
+#define A_6  GPIO_NUM_6 //ADC1
+#define A_7  GPIO_NUM_7 //ADC1
+#define A_8  GPIO_NUM_8 //ADC1
+#define A_9  GPIO_NUM_9 //ADC1
+#define A_10 GPIO_NUM_10//ADC1
+
+#define A_11 GPIO_NUM_11//ADC2
+#define A_12 GPIO_NUM_12//ADC2
+#define A_13 GPIO_NUM_13//ADC2
+#define A_14 GPIO_NUM_14//ADC2
+#define A_15 GPIO_NUM_15//ADC2
+#define A_16 GPIO_NUM_16//ADC2
+#define A_17 GPIO_NUM_17//ADC2
+#define A_18 GPIO_NUM_18//ADC2
+
+#define D_21 GPIO_NUM_21//DIGI
+#define D_22 GPIO_NUM_22//DIGI
+#define D_23 GPIO_NUM_23//DIGI
+#define D_24 GPIO_NUM_24//DIGI
+#define D_25 GPIO_NUM_25//DIGI
+#define D_26 GPIO_NUM_26//DIGI
+#define D_38 GPIO_NUM_38//DIGI
+#define D_39 GPIO_NUM_39//DIGI
+#define D_40 GPIO_NUM_40//DIGI
+#define D_41 GPIO_NUM_41//DIGI
+#define D_42 GPIO_NUM_42//DIGI
+#define D_43 GPIO_NUM_43//DIGI
+#define D_44 GPIO_NUM_44//DIGI
+#define D_47 GPIO_NUM_47//DIGI
+#define D_48 GPIO_NUM_48//DIGI
--- ESP32-S3R16V Jtag/main/usb_serial_echo_main.c
+++ ESP32-S3R16V Jtag/main/usb_serial_echo_main.c
@@ -1,9 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Unlicense OR CC0-1.0
- */
-
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -11,23 +5,26 @@
 #include "sdkconfig.h"
 #include "esp_log.h"
 #include "esp_check.h"
+#include "gpio.c"
 
-#define BUF_SIZE (1024)
-#define ECHO_TASK_STACK_SIZE (4096)
+#define LISTEN_BUF_SIZE (1024)
+#define DUMP_BUF_SIZE (1024)
+#define DUMP_TASK_STACK_SIZE (4096)
+#define LISTEN_TASK_STACK_SIZE (4096)
 
-static void echo_task(void *arg)
+static void Listen_for_Requests(void *arg)
 {
     // Configure USB SERIAL JTAG
     usb_serial_jtag_driver_config_t usb_serial_jtag_config = {
-        .rx_buffer_size = BUF_SIZE,
-        .tx_buffer_size = BUF_SIZE,
+        .rx_buffer_size =  LISTEN_BUF_SIZE,
+        .tx_buffer_size =  LISTEN_BUF_SIZE,
     };
 
     ESP_ERROR_CHECK(usb_serial_jtag_driver_install(&usb_serial_jtag_config));
     ESP_LOGI("usb_serial_jtag echo", "USB_SERIAL_JTAG init done");
 
     // Configure a temporary buffer for the incoming data
-    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
+    uint8_t *data = (uint8_t *) malloc(LISTEN_BUF_SIZE);
     if (data == NULL) {
         ESP_LOGE("usb_serial_jtag echo", "no memory for data");
         return;
@@ -35,7 +32,30 @@ static void echo_task(void *arg)
 
     while (1) {
 
-        int len = usb_serial_jtag_read_bytes(data, (BUF_SIZE - 1), 20 / portTICK_PERIOD_MS);
+        int len = usb_serial_jtag_read_bytes(data, (LISTEN_BUF_SIZE - 1), 20 / portTICK_PERIOD_MS);
+
+        // Write data back to the USB SERIAL JTAG
+        if (len) {
+            usb_serial_jtag_write_bytes((const char *) data, len, 20 / portTICK_PERIOD_MS);
+            data[len] = '\0';
+            ESP_LOG_BUFFER_HEXDUMP("Recv str: ", data, len, ESP_LOG_INFO);
+        }
+    }
+}
+
+static void Dump_Sensor_Data(void *arg)
+{
+   
+    // Configure a temporary buffer for the dump message
+    uint16_t *data = (uint8_t *) malloc(DUMP_BUF_SIZE);
+    if (data == NULL) {
+        ESP_LOGE("usb_serial_jtag echo", "no memory for data");
+        return;
+    }
+
+    while (1) {
+
+        int len = usb_serial_jtag_read_bytes(data, (DUMP_BUF_SIZE - 1), 20 / portTICK_PERIOD_MS);
 
         // Write data back to the USB SERIAL JTAG
         if (len) {
@@ -48,5 +68,6 @@ static void echo_task(void *arg)
 
 void app_main(void)
 {
-    xTaskCreate(echo_task, "USB SERIAL JTAG_echo_task", ECHO_TASK_STACK_SIZE, NULL, 10, NULL);
+    xTaskCreate(Listen_for_Requests, "USB SERIAL JTAG_listen_task", LISTEN_TASK_STACK_SIZE, NULL, 10, NULL);
+    xTaskCreate(Dump_Sensor_Data, "USB SERIAL JTAG_data_dump_task", DUMP_TASK_STACK_SIZE, NULL, 10, NULL);
 }
